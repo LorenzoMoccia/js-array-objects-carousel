954baa2b086db3ec6443a6dbe88990d3a918fd54
LorenzoMoccia <moccialorenzo98@gmail.com>
2022-11-09 19:12:27 +0100

    Clone dell'html

--- js/main.js
+++ js/main.js
@@ -24,3 +24,11 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
+
+const node = document.querySelector(".container");
+
+for(let i = 0; i < images.length; i++){
+    const clone = node.cloneNode(node);
+    document.body.appendChild(clone)
+}
